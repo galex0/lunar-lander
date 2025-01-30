056e570c36d72c6941d96431f2efd7073ed57797
Alexey Gorbunov <alexey.gorbunov07@gmail.com>
2025-01-29 22:35:05 -0800

    simple epsilon greedy policy training for simplified blackjack

--- src/env.py
+++ src/env.py
@@ -1,18 +1,73 @@
-# installing gym properly, stuck on pip install gymnasium[box2d] | conda install swig 
-# as conda hasn't downloaded yet.
-# https://www.youtube.com/watch?v=gMgj4pSHLww
+from collections import defaultdict
+import gymnasium as gym
+from matplotlib import pyplot as plt
+import numpy as np
+from tqdm import tqdm
+from agent import BlackjackAgent
 
-# installing conda, already downloaded
-# might have to enter windows and change partitions with gnome, anaconda keeps failing to download
-# https://conda.io/projects/conda/en/latest/user-guide/install/linux.html
+# hyperparameters
+learning_rate = 0.01
+n_episodes = 100_000
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)
+final_epsilon = 0.1
 
-import gym
+env = gym.make("Blackjack-v1", sab=False)
+env = gym.wrappers.RecordEpisodeStatistics(env, n_episodes)
 
-env = gym.make(
-    "LunarLander-v2",
-    continuous = False,
-    gravity = -10.0,
-    enable_wind = False,
-    wind_power = 15.0,
-    turbulence_power = 1.5,
+agent = BlackjackAgent(
+    env=env,
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
 )
+
+for episode in tqdm(range(n_episodes)):
+    obs, info = env.reset()
+    done = False
+
+    # play one episode
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, terminated, truncated, info = env.step(action)
+
+        # update the agent
+        agent.update(obs, action, reward, terminated, next_obs)
+
+        # update if the environment is done and the current obs
+        done = terminated or truncated
+        obs = next_obs
+
+    agent.decay_epsilon()
+
+# visualize the episode rewards, episode length and training error in one figure
+fig, axs = plt.subplots(1, 3, figsize=(20, 8))
+
+# np.convolve will compute the rolling mean for 100 episodes
+
+# print(np.array(env.return_queue).flatten(), "\n")
+# print(len(env.length_queue), "\n")
+# print(len(agent.training_error))
+axs[0].plot(np.convolve(np.array(env.return_queue).flatten(), np.ones(100)))
+axs[0].set_title("Episode Rewards")
+axs[0].set_xlabel("Episode")
+axs[0].set_ylabel("Reward")
+
+axs[1].plot(np.convolve(np.array(env.length_queue).flatten(), np.ones(100)))
+axs[1].set_title("Episode Lengths")
+axs[1].set_xlabel("Episode")
+axs[1].set_ylabel("Length")
+
+axs[2].plot(np.convolve(np.array(agent.training_error).flatten(), np.ones(100)))
+axs[2].set_title("Training Error")
+axs[2].set_xlabel("Episode")
+axs[2].set_ylabel("Temporal Difference")
+
+print(agent.training_error[-1])
+
+
+plt.tight_layout()
+plt.show()
+
+env.close()
